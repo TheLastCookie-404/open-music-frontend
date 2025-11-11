0359dbbc75874fe7b2ad2889008765be89054252
TheLastCookie-404 <e.sharipov@1lombard.local>
2025-11-11 15:57:47 +0500

    Typecheck fixed

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script lang="ts" setup>
+  const kek: number = "asa";
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["./app/tailwind.css"],
+  typescript: {
+    typeCheck: true,
+  },
+});
