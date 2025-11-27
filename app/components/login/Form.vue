bbd1773c9ab58607560aacb4f08571e467fa5892
TheLastCookie-404 <se660745@gmail.com>
2025-11-27 15:54:36 +0500

    feat: test auth added

--- app/components/login/Form.vue
+++ app/components/login/Form.vue
@@ -3,30 +3,66 @@
     <div class="card-body">
       <fieldset class="fieldset">
         <label class="label">Email</label>
-        <input v-model="email" type="email" class="input w-full" placeholder="Email" />
+        <input
+          v-model="userCredentials.email"
+          type="email"
+          class="input w-full"
+          placeholder="Email" />
         <label class="label">Password</label>
-        <input v-model="password" type="password" class="input w-full" placeholder="Password" />
+        <input
+          v-model="userCredentials.password"
+          type="password"
+          class="input w-full"
+          placeholder="Password" />
         <div><a class="link link-hover">Forgot password?</a></div>
-        <button class="btn btn-neutral mt-4">Login</button>
-        {{ email }} {{ password }}
+        <button @click="() => logIn()" class="btn btn-neutral mt-4">Login</button>
+        <button @click="() => test()" class="btn btn-neutral mt-4">Test</button>
+        <button @click="() => logOut()" class="btn btn-neutral mt-4">Logout</button>
+        {{ email }} {{ password }} {{ userCredentials }}
       </fieldset>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  const { $config } = useNuxtApp();
-  const env = $config.app;
-
   const email = ref<string | null>();
   const password = ref<string | null>();
 
-  async function login() {
-    const { data } = await useFetch(`${env.apiUrl}/sanctum/csrf-cookie`);
+  const { login, logout } = useSanctumAuth();
+  useSanctumClient;
 
-    const response = await $fetch(`${env.apiUrl}/login`, {
-      method: "POST",
-      body: {},
-    });
+  const userCredentials = reactive({
+    email: email.value,
+    password: password.value,
+  });
+
+  async function logIn() {
+    await login(userCredentials)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  async function test() {
+    useSanctumFetch("/api/test")
+      .then((resp) => {
+        console.log(resp.data.value);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  async function logOut() {
+    await logout()
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "nuxt-auth-sanctum"],
   vite: {
     plugins: [tailwindcss()],
     server: {
@@ -26,5 +26,13 @@ export default defineNuxtConfig({
     app: {
       apiUrl: process.env["API_URL"],
     },
+  },
+  sanctum: {
+    baseUrl: "http://localhost:8000",
+    mode: "cookie",
+    endpoints: {
+      login: "/api/login",
+    },
+    // mode: "cookie",
   },
 });
